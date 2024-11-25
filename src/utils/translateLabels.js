dbbe387f3cb9a3b681f6357b5ea9324d0ed345ce
Marcio <marciodosanjos@ymail.com>
2024-11-25 08:00:41 -0300

    redesign list products on admin page

--- src/utils/translateLabels.js
+++ src/utils/translateLabels.js
@@ -24,6 +24,30 @@ const translateLabels = (value) => {
     case "maxSale":
       return "Máx de itens p/ compra";
       break;
+    case "brand":
+      return "Marca";
+      break;
+    case "name":
+      return "Nome";
+      break;
+    case "price":
+      return "Preço";
+      break;
+    case "images":
+      return "Imagens";
+      break;
+    case "sizes":
+      return "Tamanhos";
+      break;
+    case "totalQty":
+      return "Quantidade";
+      break;
+    case "colors":
+      return "Cores";
+      break;
+    case "category":
+      return "Categoria";
+      break;
     default:
       return "outro";
       break;
