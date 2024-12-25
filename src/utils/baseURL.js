a9e4a9216ed6b4d64994401a56f9cdc68aac9cb6
Marcio dos Anjos <marciodosanjos@ymail.com>
2024-12-25 10:23:00 +0100

    new base url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const baseURL = "https://garimpaae.onrender.com/api/v1";
+const baseURL = "https://garimpaae-api.vercel.app/api/v1";
 //const baseURL = "http://localhost:7000/api/v1";
 
 export default baseURL;
